88fd0e9bdb56436db796169a937c9d2f03c166e3
Qwelice <kamilzalyalov00@gmail.com>
2021-10-22 13:22:24 +0300

    init

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            new Bot("ODkzNjE5MTY1NjgyODkyODUy.YVeFsw.4s9hgJ4aVPHXB3r4Xn93rO2Zwqs")
+            new Bot("YOUR TOKEN")
                 .Start()
                 .GetAwaiter()
                 .GetResult();
